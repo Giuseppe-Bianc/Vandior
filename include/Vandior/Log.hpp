7b1cb9cccd5727fa472ebe6fea92702439f5342d
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-12-27 17:03:00 +0100

    modifice

--- include/Vandior/Log.hpp
+++ include/Vandior/Log.hpp
@@ -3,6 +3,18 @@
 #include <Vandior/vandior_export.hpp>
 #include "disableWarn.hpp"
 
+#pragma once
+
+#if defined(__clang__)
+// Clang/LLVM
+#elif defined(__GNUC__) || defined(__GNUG__)
+// GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wignored-optimization-argument"
+#pragma GCC diagnostic ignored "-Wunused-command-line-argument"
+#elif defined(_MSC_VER)
+// MSVC
 DISABLE_WARNINGS_PUSH(
     4005 4201 4459 4514 4625 4626 4820
     6244 6285 6385 6386 26409 26415 26418
@@ -11,11 +23,28 @@ DISABLE_WARNINGS_PUSH(
     26467 26472 26473 26474 26475 26481 26482
     26485 26490 26491 26493 26494 26495 26496
     26497 26498 26800 26814 26818 26826)
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+// MinGW
+#else
+// Unknown compiler
+#endif
 
 
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
+#if defined(__clang__)
+// Clang/LLVM
+#elif defined(__GNUC__) || defined(__GNUG__)
+// GCC
+#pragma GCC diagnostic pop // Restore warning settings
+#elif defined(_MSC_VER)
+// MSVC
 DISABLE_WARNINGS_POP()
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+// MinGW
+#else
+// Unknown compiler
+#endif
 
 #define LTRACE(...) SPDLOG_TRACE(__VA_ARGS__)
 #define LDEBUG(...) SPDLOG_DEBUG(__VA_ARGS__)
