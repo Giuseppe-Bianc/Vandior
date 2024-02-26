93133226e12ebd1f8486ed7039cd6ab358036fda
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2024-02-26 17:33:25 +0100

    fix: fix some erors on linux like python instead of python3

--- src/Vandior/main.cpp
+++ src/Vandior/main.cpp
@@ -62,8 +62,8 @@ constexpr std::string_view filename = "../../../../input.vn";  // windows mingw 
 constexpr std::string_view filename = "../../../input.vn";
 #endif
 #elif defined __unix__  // Linux and Unix-like systems
-constexpr std::string_view filename = "../../../../input.vn";  // Linux and Unix  form editor
-//constexpr std::string_view filename = "../../../input.vn";  // Linux and Unix
+//constexpr std::string_view filename = "../../../../input.vn";  // Linux and Unix  form editor
+constexpr std::string_view filename = "../../../input.vn";  // Linux and Unix
 #endif
 auto extractInstructions(const std::vector<vnd::Token> &tokens) -> std::vector<vnd::Instruction> {
     std::vector<vnd::Instruction> instructions;
