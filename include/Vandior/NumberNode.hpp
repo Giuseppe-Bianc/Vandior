a8690a2adf3f8c31d481f6ac5b5ae58d107f047a
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2024-05-02 12:42:42 +0200

    refactoring reduced commented code

--- include/Vandior/NumberNode.hpp
+++ include/Vandior/NumberNode.hpp
@@ -12,26 +12,8 @@ namespace vnd {
      */
     class NumberNode : public ASTNode {
     public:
-        /**
-         * @brief Constructor for NumberNode with integer value.
-         * @param _value Integer value of the number.
-         */
-        // explicit NumberNode(int _value) noexcept : value(C_D(_value)) {}
-        /**
-         * @brief Constructor for NumberNode with double value.
-         * @param _value Double value of the number.
-         */
-        // explicit NumberNode(double _value) noexcept : value(_value) {}
-
         [[nodiscard]] NodeType getType() const noexcept override { return NodeType::Number; }
         [[nodiscard]] virtual NumberNodeType getNumberType() const = 0;
-
-        /*[[nodiscard]] std::string print() const override { return FORMAT("{}({})", getType(), value); }
-        [[nodiscard]] std::string comp_print() const override { return FORMAT("NUM({})", value); }
-        [[nodiscard]] double getValue() const noexcept { return value; }*/
-
-    private:
-        // double value;
     };
 
 }  // namespace vnd
