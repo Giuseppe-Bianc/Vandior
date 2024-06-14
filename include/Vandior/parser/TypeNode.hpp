4e3cf11cf12f53942b38182b1f373cb61442cfa4
potex02 <roccopiopz@gmail.com>
2024-06-14 14:56:58 +0200

    Added vnd::TypeToken

---
+++ include/Vandior/parser/TypeNode.hpp
@@ -0,0 +1,60 @@
+//
+// Created by potex02 on 14/05/2024.
+//
+
+#pragma once
+#include "ASTNode.hpp"
+
+namespace vnd {
+
+    /**
+     * @brief Node class representing type value in the AST.
+     */
+    class TypeNode : public ASTNode {
+    public:
+        /**
+         * @brief Creates a TypeNode.
+         * @param token The token correspondent to the node.
+         */
+        [[nodiscard]] TypeNode(Token token) noexcept: ASTNode(token), m_value(token.getValue()), m_type(token.getType()) {}
+        
+        /**
+         * @brief Gets the type of the AST node.
+         * @return NodeType enumeration value.
+         */
+        [[nodiscard]] NodeType getType() const noexcept override { return NodeType::Type; }
+
+        /**
+         * @brief Returns a string representation of the AST node.
+         * @return String representation of the AST node.
+         */
+        [[nodiscard]] std::string print() const override {
+            return FORMAT("{}_{}({})", m_type, getType(), get_value());
+        }
+
+        /**
+         * @brief Returns a compact string representation of the AST node for compilation purposes.
+         * @return Compact string representation of the AST node.
+         */
+        [[nodiscard]] std::string comp_print() const override { return FORMAT("{}({})", getType(), get_value()); }
+
+        /**
+         * @brief Gets the value of the node.
+         * @return The value of the node.
+         */
+        [[nodiscard]] std::string_view get_value() const noexcept { return m_value; }
+
+        friend void swap(TypeNode &lhs, TypeNode &rhs) noexcept {
+            using std::swap;
+            swap(static_cast<TypeNode &>(lhs), static_cast<TypeNode &>(rhs));
+            swap(lhs.m_value, rhs.m_value);
+            swap(lhs.m_type, rhs.m_type);
+        }
+
+    private:
+        std::string_view m_value;
+        TokenType m_type;
+
+    };
+
+}  // namespace vnd
