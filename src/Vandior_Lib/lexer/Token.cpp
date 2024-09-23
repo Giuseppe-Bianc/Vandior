bc624cfb963943e2fffdbccfc0a099a93a095bed
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2024-09-23 22:38:44 +0200

    general refactoring of Token.cpp

--- src/Vandior_Lib/lexer/Token.cpp
+++ src/Vandior_Lib/lexer/Token.cpp
@@ -1,12 +1,14 @@
 #include "Vandior/lexer/Token.hpp"
 // NOLINTBEGIN(*-include-cleaner)
-std::string vnd::Token::to_string() const {
-    if(_type == eofTokenType) { return FORMAT("(type: {}, source location:{})", _type, _sourceLocation); }
-    return FORMAT("(type: {}, value: '{}', source location:{})", _type, _value, _sourceLocation);
-}
+namespace vnd {
+    std::string Token::to_string() const {
+        if(_type == eofTokenType) { return FORMAT("(type: {}, source location:{})", _type, _sourceLocation); }
+        return FORMAT("(type: {}, value: '{}', source location:{})", _type, _value, _sourceLocation);
+    }
 
-std::string vnd::Token::compat_to_string() const {
-    if(_type == eofTokenType) { return FORMAT("(typ: {}, sl:{})", comp_tokType(_type), _sourceLocation.compat_to_string()); }
-    return FORMAT("(typ: {}, val: '{}', sl:{})", comp_tokType(_type), _value, _sourceLocation.compat_to_string());
-}
+    std::string Token::compat_to_string() const {
+        if(_type == eofTokenType) { return FORMAT("(typ: {}, sl:{})", comp_tokType(_type), _sourceLocation.compat_to_string()); }
+        return FORMAT("(typ: {}, val: '{}', sl:{})", comp_tokType(_type), _value, _sourceLocation.compat_to_string());
+    }
+}  // namespace vnd
 // NOLINTEND(*-include-cleaner)
