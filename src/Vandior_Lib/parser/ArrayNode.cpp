0c96e64d9ebfff0a5c773aefc3f1ec7bfb71a1da
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2024-10-22 21:16:03 +0200

    move  implementation of ArrayNode to the  .cpp file
    Signed-off-by: GIUSEPPE BIANCONI <gbianconi3@gmail.com>

--- src/Vandior_Lib/parser/ArrayNode.cpp
+++ src/Vandior_Lib/parser/ArrayNode.cpp
@@ -5,5 +5,20 @@
 #include "Vandior/parser/ArrayNode.hpp"
 
 namespace vnd {
+    ArrayNode::ArrayNode(std::unique_ptr<ASTNode> elements, const Token &token) noexcept
+          : ASTNode(token), m_elements(vnd_move_always_even_const(elements)) {
+        if(m_elements){ m_elements->set_parent(this);}
+    }
+
+    NodeType ArrayNode::getType() const noexcept { return NodeType::Array; }
+
+    std::string ArrayNode::print() const {
+        if(m_elements) { return FORMAT("{}({})", getType(), m_elements->comp_print()); }
+        return FORMAT("{}()", getType());
+    }
+
+    std::string ArrayNode::comp_print() const { return FORMAT("{}", getType()); }
+
+    const std::unique_ptr<ASTNode> &ArrayNode::get_elements() const noexcept { return m_elements; }
 }  // namespace vnd
    // NOLINTEND(*-include-cleaner)
